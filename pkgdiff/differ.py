8934407bcc439ec5bc8a63d4e6b14f6d5da0ee62
conni <cbuschka@gmail.com>
2021-08-19 22:43:32 +0200

    Improved handling of missing packages.

--- pkgdiff/differ.py
+++ pkgdiff/differ.py
@@ -1,4 +1,7 @@
 def create_diff(listA, listB, *, aExcludes=None, bExcludes=None):
+  aExcludes = aExcludes or []
+  bExcludes = bExcludes or []
+
   mapA = {p.name: p for p in listA}
   mapB = {p.name: p for p in listB}
   diffAB = {}
@@ -6,14 +9,14 @@ def create_diff(listA, listB, *, aExcludes=None, bExcludes=None):
     packageA = mapA[packageName]
     packageB = mapB.get(packageName, None)
     if packageB is None:
-      if bExcludes is None or "missing" not in bExcludes:
+      if "missing" not in bExcludes:
         diffAB[packageName] = [packageA.version, "missing"]
     elif packageB != packageA:
       diffAB[packageName] = [packageA.version, packageB.version]
   for packageName in mapB:
     if not packageName in diffAB:
       packageB = mapB[packageName]
-      if aExcludes is None or "missing" not in aExcludes:
+      if "missing" not in aExcludes:
         diffAB[packageName] = ["missing", packageB.version]
 
   return diffAB
--- tests/differ_test.py
+++ tests/differ_test.py
@@ -19,3 +19,33 @@ class DifferTest(unittest.TestCase):
                       'c': ['v1-1.0', 'v1-1.1'],
                       'd': ['v1-1.0', 'v2-1.0']},
                      diff)
+
+  def test_exclude_source(self):
+    a = Package("a", "v1-1.0")
+    b = Package("b", "v1-1.0")
+    c1 = Package("c", "v1-1.0")
+    c2 = Package("c", "v1-1.1")
+    d1 = Package("d", "v1-1.0")
+    d2 = Package("d", "v2-1.0")
+
+    diff = create_diff([a, c1, d1], [b, c2, d2], aExcludes=["missing"])
+
+    self.assertEqual({'a': ['v1-1.0', 'missing'],
+                      'c': ['v1-1.0', 'v1-1.1'],
+                      'd': ['v1-1.0', 'v2-1.0']},
+                     diff)
+
+  def test_exclude_target(self):
+    a = Package("a", "v1-1.0")
+    b = Package("b", "v1-1.0")
+    c1 = Package("c", "v1-1.0")
+    c2 = Package("c", "v1-1.1")
+    d1 = Package("d", "v1-1.0")
+    d2 = Package("d", "v2-1.0")
+
+    diff = create_diff([a, c1, d1], [b, c2, d2], bExcludes=["missing"])
+
+    self.assertEqual({'b': ['missing', 'v1-1.0'],
+                      'c': ['v1-1.0', 'v1-1.1'],
+                      'd': ['v1-1.0', 'v2-1.0']},
+                     diff)
