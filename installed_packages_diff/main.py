ce02b2dc56631c4250b2b1f261ac31f8e7dca49b
conni <cbuschka@gmail.com>
2021-08-26 06:57:56 +0200

    Added options parsing.

--- installed_packages_diff/main.py
+++ installed_packages_diff/main.py
@@ -1,3 +1,4 @@
+from optparse import OptionParser
 import sys
 
 from .config import load_config
@@ -14,17 +15,37 @@ def diff_server(serverA, serverB):
   prodPkgs = pkgFetcher.get_packages(serverB.hostname,
                                      username=serverB.username,
                                      type=serverB.type)
-  installed_packages_diff = create_diff(devPkgs, prodPkgs, aExcludes=serverA.excludes,
-                        bExcludes=serverB.excludes, includeEqual=False)
+  installed_packages_diff = create_diff(devPkgs, prodPkgs,
+                                        aExcludes=serverA.excludes,
+                                        bExcludes=serverB.excludes,
+                                        includeEqual=False)
   print_diff(serverA.hostname, serverB.hostname, installed_packages_diff)
+
+
+def _parse_args():
+  opt_parser = OptionParser()
+  opt_parser.add_option("-c", "--config", dest="config_file",
+                        help="Path to config file", metavar="CONFIG_FILE",
+                        default="./config.yaml")
+  opt_parser.add_option("-v", "--verbose", dest="verbose",
+                        help="Increase output", metavar="VERBOSE",
+                        default=True)
+
+  options, args = opt_parser.parse_args()
+  option_vars = vars(options)
+  return (option_vars, args)
 
 
 def main():
   logging.basicConfig(stream=sys.stderr, encoding='utf-8',
                       format='%(asctime)s %(message)s',
-                      level=logging.WARN)
+                      level=logging.INFO)
 
-  config = load_config(sys.argv[1] if len(sys.argv) > 1 else "config.yaml")
+  option_vars, args = _parse_args()
+  logging.getLogger().setLevel(
+    logging.INFO if option_vars["verbose"] else logging.WARN)
+
+  config = load_config(option_vars.get("config_file"))
   for group in config.groups:
     serverA = group.servers[0]
     serverB = group.servers[1]
