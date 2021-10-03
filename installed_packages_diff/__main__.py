2b6d4c5476453e36b78707a5f6263c7d89652285
conni <cbuschka@gmail.com>
2021-10-03 16:43:40 +0200

    Removed python stuff.

--- installed_packages_diff/__main__.py
+++
@@ -1,4 +0,0 @@
-from installed_packages_diff.main import main
-
-if __name__ == "__main__":
-  main()
--- installed_packages_diff/printing.py
+++
@@ -1,5 +0,0 @@
-def print_diff(serverA, serverB, installed_packages_diff, *, file=None):
-  print(f"\n= {serverA} {serverB} =", file=file)
-  for (packageName, versionA, versionB) in installed_packages_diff:
-    print(f"{packageName:40s} {versionA:40s} {versionB:40s}",
-          file=file)
